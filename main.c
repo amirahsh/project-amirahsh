af0626b730a3e3ec2ee56bd43a518635f6db2a65
amirahsh <amirhoseinshahidi20041382@gmail.com>
2023-01-31 17:39:09 +0330

    createfile,insert,cat,remove,copy,cut completed

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 #include<stdlib.h>
 #include<dir.h>
 #include<string.h>
+char copystr[300];
+char cutstr[300];
 void inputcommand(char string[]){
     scanf("%s",string);
 }
@@ -222,7 +224,6 @@ void fileinserterspace() {
                                 printf("Not successful to move pointer\n");
                             else {
                                 if (!fputs(text, ptr))
-
                                     printf("Successful\n");
                                 else
                                     printf("Unsuccessful\n");
@@ -332,7 +333,6 @@ void fileinserterspace() {
                                          printf("Not successful to move pointer\n");
                                      else {
                                          if (!fputs(text, ptr))
-
                                              printf("Successful\n");
                                          else
                                              printf("Unsuccessful\n");
@@ -415,7 +415,7 @@ int removex(){
     char dir1[30]="C:\\Users\\Amirhosein\\";
     char dir[30] = "C:\\Users\\Amirhosein";
     char * ourroot;
-    long int line,pos,entercounter=1,entercounter2=0,entercounter3=1,size=0,sizego;
+    long int line,pos,entercounter=1,entercounter2=0,entercounter3=0,size=0,sizego,entercounter4=1;
     c=getcharx();
     //printf("%c",c);
     if(c=='\\') {
@@ -450,28 +450,36 @@ int removex(){
         //printf("%ld %ld %ld %ld %ld %c %c", ftell(ptr),line,pos,entercounter2,entercounter,e,way);
         if(entercounter<line)
             return 3;
+        entercounter3= ftell(ptr);
         rewind(ptr);
-
         while (fgets(saver, 200, ptr) != NULL) {
-            if (entercounter3 == line) {
-                size = strlen(saver);
-                //printf(" %d",size);
+            if (entercounter4 == line) {
+                size = ftell(ptr)-entercounter4 - size;
+                //printf("%d", size);
                 break;
-            } else
-                entercounter3++;
+            } else {
+                size = ftell(ptr)-entercounter4;
+                // printf("%d  ",size);
+                entercounter4++;
+            }
         }
         if(pos>size)
             return 4;
-        if(fseek(ptr, 0 , SEEK_SET)!=0)
-            return 6;
         long int j = 0;
+        rewind(ptr);
+        int x=0;
+       // printf("%d %d %d",pos,sizego,size,entercounter4);
         if(way=='f') {
-            for (long int i =0; i < entercounter2+pos && (c = fgetc(ptr)) != EOF; ++i) {
+            if(entercounter2 + pos + sizego>entercounter3){
+                return 5;}
+            if(pos + sizego>=size)
+                x+=entercounter4;
+            for (long int i = 0; i < entercounter2 + pos-line+1 && (c = fgetc(ptr)) != EOF; ++i) {
                 saver2[j++] = c;
             }
-            saver2[j]='\0';
+            saver2[j] = '\0';
             long int j = 0;
-            if(fseek(ptr, entercounter2 + pos+sizego, SEEK_SET)!=0)
+            if (fseek(ptr, entercounter2 + pos + sizego+x, SEEK_SET) != 0)
                 return 5;
             for (;  (c = fgetc(ptr)) != EOF; ) {
                 saver3[j++] = c;
@@ -481,10 +489,16 @@ int removex(){
             fclose(ptr);
             ptr=fopen(ourroot,"w");
             //printf("%s",whole);
-            fputs(whole,ptr);
-            fclose(ptr);
+            if (!fputs(whole, ptr)) {
+                fclose(ptr);
+                return 7;
+            }
+            else {
+                fclose(ptr);
+                return 8;
+            }
         }
-        else if(way='b') {
+        else if(way=='b') {
             if(entercounter2-sizego+pos<0)
                 return 5;
             for (long int i =0; i < entercounter2-sizego+pos-1 && (c = fgetc(ptr)) != EOF; ++i) {
@@ -502,11 +516,19 @@ int removex(){
             fclose(ptr);
             ptr=fopen(ourroot,"w");
             //printf("%s",whole);
-            fputs(whole,ptr);
-            fclose(ptr);
+            if (!fputs(whole, ptr)) {
+                fclose(ptr);
+                return 7;
+            }
+            else {
+                fclose(ptr);
+                return 8;
+            }
         }
-        else
-            printf("Invalid input\n");
+        else {
+            fclose(ptr);
+            return 2;
+        }
     }
     else  if(c=='"') {
         getcot(root);
@@ -516,18 +538,390 @@ int removex(){
         if (ptr == NULL) {
            return 1;
         }
+        inputcommand(com);
+        // printf("%s",com);
+        if(strcmp(com,"--pos")!=0){
+            return 2;
+        }
+        scanf("%ld%c%ld", &line, &s, &pos);
+        inputcommand(com);
+        // printf("%s",com);
+        if(strcmp(com,"-size")!=0){
+            return 2;
+        }
+        scanf("%d",&sizego);
+        getcharx();
+        e=getcharx();
+        way=getcharx();
+        while (fgets(saver, 200, ptr) != NULL) {
+            if(entercounter<line) {
+                entercounter2 += strlen(saver)+1;
+            }
+            entercounter++;
+        }
+        //printf("%ld %ld %ld %ld %ld %c %c", ftell(ptr),line,pos,entercounter2,entercounter,e,way);
+        if(entercounter<line)
+            return 3;
+        entercounter3= ftell(ptr);
+        rewind(ptr);
+        while (fgets(saver, 200, ptr) != NULL) {
+            if (entercounter4 == line) {
+                size = ftell(ptr)-entercounter4 - size;
+                //printf("%d", size);
+                break;
+            } else {
+                size = ftell(ptr)-entercounter4;
+                // printf("%d  ",size);
+                entercounter4++;
+            }
+        }
+        if(pos>size)
+            return 4;
+        long int j = 0;
+        rewind(ptr);
+        int x=0;
+        if(way=='f') {
+            if(entercounter2 + pos + sizego>entercounter3){
+                return 5;}
+            if(pos + sizego>=size)
+                x+=entercounter4;
+            for (long int i = 0; i < entercounter2 + pos-line+1 && (c = fgetc(ptr)) != EOF; ++i) {
+                saver2[j++] = c;
+            }
+            saver2[j] = '\0';
+            long int j = 0;
+            if (fseek(ptr, entercounter2 + pos + sizego+x, SEEK_SET) != 0)
+                return 5;
+            for (;  (c = fgetc(ptr)) != EOF; ) {
+                saver3[j++] = c;
+            }
+            saver3[j] = '\0';
+            whole = strcat(saver2, saver3);
+            fclose(ptr);
+            ptr=fopen(ourroot,"w");
+            //printf("%s",whole);
+            if (!fputs(whole, ptr)) {
+                fclose(ptr);
+                return 7;
+            }
+            else {
+                fclose(ptr);
+                return 8;
+            }
+        }
+        else if(way=='b') {
+            if(entercounter2-sizego+pos<0)
+                return 5;
+            for (long int i =0; i < entercounter2-sizego+pos-1 && (c = fgetc(ptr)) != EOF; ++i) {
+                saver2[j++] = c;
+            }
+            saver2[j]='\0';
+            long int j = 0;
+            if(fseek(ptr, entercounter2 + pos, SEEK_SET)!=0)
+                return 5;
+            for (;  (c = fgetc(ptr)) != EOF; ) {
+                saver3[j++] = c;
+            }
+            saver3[j] = '\0';
+            whole = strcat(saver2, saver3);
+            fclose(ptr);
+            ptr=fopen(ourroot,"w");
+            //printf("%s",whole);
+            if (!fputs(whole, ptr)) {
+                fclose(ptr);
+                return 7;
+            }
+            else {
+                fclose(ptr);
+                return 8;
+            }
 
-        fclose(ptr);
+        }
+        else {
+            fclose(ptr);
+            return 2;
+        }
+
+
     }
 }
 
+int copy() {
+    char c, s, e, way;
+    char root[70] = {0}, com[15], saver[200];
+    char dir1[30] = "C:\\Users\\Amirhosein\\";
+    char dir[30] = "C:\\Users\\Amirhosein";
+    char *ourroot;
+    FILE *ptr;
+    long int line, pos, entercounter = 1, entercounter2 = 0, entercounter3 = 0, size = 0, sizego,entercounter4=0;
+    c = getcharx();
+    //printf("%c",c);
+    if (c == '\\') {
+        inputcommand(root);
+        ourroot = strcat(dir1, root);
+        // printf("%s",ourroot);
+        ptr = fopen(ourroot, "r");
+        if (ptr == NULL) {
+            return 1;
+        }
+    } else if (c == '"') {
+        getcot(root);
+        ourroot = strcat(dir, root);
+        // printf("%s %s",ourroot,root);
+        ptr = fopen(ourroot, "r");
+        if (ptr == NULL) {
+            return 1;
+        }
+    }
+    inputcommand(com);
+    // printf(" %s",com);
+    if (strcmp(com, "--pos") != 0) {
+        return 2;
+    }
+    scanf("%ld%c%ld", &line, &s, &pos);
+    inputcommand(com);
+    //printf(" %s",com);
+    if (strcmp(com, "-size") != 0) {
+        return 2;
+    }
+    scanf("%d", &sizego);
+    getcharx();
+    e = getcharx();
+    way = getcharx();
+    while (fgets(saver, 200, ptr) != NULL) {
+        if (entercounter < line) {
+            entercounter2 += strlen(saver) + 1;
+        }
+
+        entercounter++;
+    }
+
+    long int j = 0;
+    // printf(" %ld %ld %ld %ld %ld %c %c", ftell(ptr),line,pos,entercounter2,entercounter,e,way);
+    if (entercounter < line)
+        return 3;
+    entercounter3= ftell(ptr);
+    rewind(ptr);
+    while (fgets(saver, 200, ptr) != NULL) {
+        if (entercounter4 == line) {
+            size = strlen(saver);
+            //printf(" %d",size);
+            break;
+        } else
+            entercounter4++;
+    }
+    if(pos>size)
+        return 4;
+        if (way == 'f') {
+            if (entercounter2 + pos + sizego > entercounter3)
+                return 5;
+            if (fseek(ptr, entercounter2 + pos + sizego, SEEK_SET) != 0)
+                return 5;
+            if (fseek(ptr, entercounter2 + pos, SEEK_SET) != 0)
+                return 5;
+            for (long int i = 0; i < sizego && (c = fgetc(ptr)) != EOF; ++i) {
+                copystr[j++] = c;
+            }
+            copystr[j] = '\0';
+            j = 0;
+
+        } else if (way == 'b') {
+            if (entercounter2 - sizego + pos < 0)
+                return 5;
+            if (fseek(ptr, entercounter2 + pos - sizego - 1, SEEK_SET) != 0)
+                return 5;
+            for (long int i = 0; i < sizego && (c = fgetc(ptr)) != EOF; ++i) {
+                copystr[j++] = c;
+            }
+            copystr[j] = '\0';
+            j = 0;
+        }
+        fclose(ptr);
+        //printf("%s",copystr);
+        return 7;
+}
+int cut(){
+    char c,s,e,way;
+    char root[70]={0},com[15],*whole,saver[200],saver2[1000],saver3[1000];
+    char dir1[30]="C:\\Users\\Amirhosein\\";
+    char dir[30] = "C:\\Users\\Amirhosein";
+    char * ourroot;
+    FILE * ptr;
+    long int line,pos,entercounter=1,entercounter2=0,entercounter3=0,size=0,sizego,entercounter4=1;
+    c=getcharx();
+    //printf("%c",c);
+    if(c=='\\') {
+        inputcommand(root);
+        ourroot = strcat(dir1, root);
+        // printf("%s",ourroot);
+        ptr = fopen(ourroot, "r");
+        if (ptr == NULL) {
+            return 1;
+        }
+    }
+    else  if(c=='"') {
+        getcot(root);
+        ourroot = strcat(dir, root);
+        // printf("%s %s",ourroot,root);
+        ptr = fopen(ourroot, "r");
+        if (ptr == NULL) {
+            return 1;
+        }
+    }
+    inputcommand(com);
+    // printf(" %s",com);
+    if (strcmp(com, "--pos") != 0) {
+        return 2;
+    }
+    scanf("%ld%c%ld", &line, &s, &pos);
+    inputcommand(com);
+    //printf(" %s",com);
+    if (strcmp(com, "-size") != 0) {
+        return 2;
+    }
+    scanf("%d", &sizego);
+    getcharx();
+    e = getcharx();
+    way = getcharx();
+    while (fgets(saver, 200, ptr) != NULL) {
+        if (entercounter < line) {
+            entercounter2 += strlen(saver) + 1;
+        }
+        entercounter++;
+    }
+    // printf(" %ld %ld %ld %ld %ld %c %c %ld %ld", ftell(ptr),line,pos,entercounter2,entercounter,e,way,entercounter3,sizego);
+    if ( entercounter < line)
+        return 3;
+    entercounter3= ftell(ptr);
+    rewind(ptr);
+    while (fgets(saver, 200, ptr) != NULL) {
+        if (entercounter4 == line) {
+            size = ftell(ptr)-entercounter4 - size;
+            //printf("%d", size);
+            break;
+        } else {
+            size = ftell(ptr)-entercounter4;
+           // printf("%d  ",size);
+            entercounter4++;
+        }
+    }
+    if(pos>size)
+        return 4;
+    long int j = 0;
+    rewind(ptr);
+    int x=0;
+    if(way=='f') {
+        if(entercounter2 + pos + sizego>entercounter3){
+            return 5;}
+        if(pos + sizego>=size)
+            x+=entercounter4;
+        for (long int i = 0; i < entercounter2 + pos-line+1 && (c = fgetc(ptr)) != EOF; ++i) {
+            saver2[j++] = c;
+        }
+        saver2[j] = '\0';
+        long int j = 0;
+        if (fseek(ptr, entercounter2 + pos + sizego+x, SEEK_SET) != 0)
+            return 5;
+        for (; (c = fgetc(ptr)) != EOF;) {
+            saver3[j++] = c;
+        }
+        saver3[j] = '\0';
+        //printf("%s",saver3);
+        whole = strcat(saver2, saver3);
+        if(fseek(ptr, entercounter2 + pos+sizego, SEEK_SET)!=0)
+            return 5;
+        if (fseek(ptr, entercounter2 + pos , SEEK_SET) != 0)
+            return 5;
+        j=0;
+        for (long int i = 0; i < sizego && (c = fgetc(ptr)) != EOF; ++i) {
+            cutstr[j++] = c;
+        }
+        cutstr[j] = '\0';
+         j = 0;
+        fclose(ptr);
+        ptr = fopen(ourroot, "w");
+        //printf("%s",cutstr);
+        if (!fputs(whole, ptr)) {
+            fclose(ptr);
+            return 7;
+        } else {
+            fclose(ptr);
+            return 8;
+        }
+    }
+    else if(way=='b') {
+        if(entercounter2-sizego+pos<0)
+            return 5;
+        for (long int i =0; i < entercounter2-sizego+pos-1 && (c = fgetc(ptr)) != EOF; ++i) {
+            saver2[j++] = c;
+        }
+        saver2[j]='\0';
+        long int j = 0;
+        if(fseek(ptr, entercounter2 + pos, SEEK_SET)!=0)
+            return 5;
+        for (;  (c = fgetc(ptr)) != EOF; ) {
+            saver3[j++] = c;
+        }
+        saver3[j] = '\0';
+        whole = strcat(saver2, saver3);
+        if (entercounter2 - sizego + pos < 0)
+            return 5;
+        if (fseek(ptr, entercounter2 + pos -sizego-1 , SEEK_SET) != 0)
+            return 5;
+        j=0;
+        for (long int i = 0; i <  sizego  && (c = fgetc(ptr)) != EOF; ++i) {
+            cutstr[j++] = c;
+        }
+        cutstr[j] = '\0';
+        j = 0;
+        fclose(ptr);
+        ptr=fopen(ourroot,"w");
+        //printf("%s",cutstr);
+        if (!fputs(whole, ptr)) {
+            fclose(ptr);
+            return 7;
+        }
+        else {
+            fclose(ptr);
+            return 8;
+        }
+    }
+}
+void switchx(int answer){
+    switch (answer) {
+        case 1:
+            printf("File could not be created or be opened try again\n");
+            break;
+        case 2:
+            printf("Invalid input try again\n");
+            break;
+        case 3:
+            printf("You cant do this action in that line\n");
+            break;
+        case 4:
+            printf("You cant do this action in that position\n");
+            break;
+        case 5:
+            printf("You cannot move that much\n");
+            break;
+        case 6:
+            printf("Not successful to move pointer\n");
+            break;
+        case 7:
+            printf("Successful\n");
+            break;
+        case 8:
+            printf("Unsuccessful\n");
+            break;
+    }
+}
 int main() {
     char com[15],com2[8],c;
     int answer=-1;
-    printf("User Guide:\n A:for creating file or folder:\n   1-createfile --file <file name and address>\n   2-createfile --file <\"file name and address\">"
-           "(if your folder or your filenames have space\n   *Note:if you are willing to create folders only type '\\' at the end\n   Example:\\root\\1\\abc.txt\n"
-           " B:insertfile:\n    1-insertstr --file <file name and address> –str <str> --pos <line no>:<start position>\n    2-You can enter string or filename with cotation same as createfile"
-           "command\n C:Showing the content of a file:\n   1-cat --file <file name and address>\n");
+//    printf("User Guide:\n A:for creating file or folder:\n   1-createfile --file <file name and address>\n   2-createfile --file <\"file name and address\">"
+//           "(if your folder or your filenames have space\n   *Note:if you are willing to create folders only type '\\' at the end\n   Example:\\root\\1\\abc.txt\n"
+//           " B:insertfile:\n    1-insertstr --file <file name and address> –str <str> --pos <line no>:<start position>\n    2-You can enter string or filename with cotation same as createfile"
+//           "command\n C:Showing the content of a file:\n   1-cat --file <file name and address>\n");
     inputcommand(com);
     while(strcmp(com,"exit")!=0){
         if(strcmp(com,"createfile")==0){
@@ -572,35 +966,36 @@ int main() {
             getcharx();
             if(strcmp(com2,"--file")==0){
                 answer=removex();
-                switch (answer) {
-                    case 1:
-                        printf("File could not be created or be opened try again\n");
-                        break;
-                    case 2:
-                        printf("Invalid input try again\n");
-                        break;
-                    case 3:
-                        printf("You cant do this action in that line\n");
-                        break;
-                    case 4:
-                        printf("You cant do this action in that position\n");
-                        break;
-                    case 5:
-                        printf("You cannot move that much\n");
-                        break;
-                    case 6:
-                        printf("Not successful to move pointer\n");
-                        break;
-                }
+                switchx(answer);
             }
             else
                 printf("Invalid input\n");
         }
+        else if(strcmp(com,"copystr")==0) {
+            inputcommand(com2);
+            getcharx();
+            if (strcmp(com2, "--file") == 0) {
+                answer = copy();
+                switchx(answer);
+            }
+            else
+                printf("Invalid input\n");
+        }
+        else if(strcmp(com,"cutstr")==0) {
+            inputcommand(com2);
+            getcharx();
+            if (strcmp(com2, "--file") == 0) {
+                answer = cut();
+                switchx(answer);
+            } else
+                printf("Invalid input\n");
+        }
         else {
             if(getcharx()=='\n')
-                printf("Invalid Input");
+                printf("Invalid Input\n");
         }
         inputcommand(com);
     }
     return 0;
 }
+
